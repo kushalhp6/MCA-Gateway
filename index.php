0e5e8e1d2d8e3de24ecbf539658ea9f604fbad61
Kushal Dalal <kdalalhp@gmail.com>
2024-08-16 20:48:05 +0530

    moving index file from views to root

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include 'views/partials/navbar.php'; ?>
+<h1>Welcome to MCA Gateway</h1>
+<p>Your gateway to MCA preparation.</p>
+<?php include 'views/partials/footer.php'; ?>
--- views/index.php
+++
@@ -1,4 +0,0 @@
-<?php include 'partials/navbar.php'; ?>
-<h1>Welcome to MCA Gateway</h1>
-<p>Your gateway to MCA preparation.</p>
-<?php include 'partials/footer.php'; ?>
